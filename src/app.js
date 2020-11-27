e09b371db158e464bbee2c502b0552719e6eaced
sowjanyamanugula <manugulasowjanya@gmail.com>
2020-11-27 14:43:40 +0530

    fixed module1 errors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const app=express();
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static('public'));
+//app.use(express.static('public',['js']));
 
 app.get('/',function(req,res){
   res.render('index',{
@@ -14,5 +16,6 @@ app.get('/',function(req,res){
   });
 });
 
-app.listen(3000);
-console.log('PS Project Running on port 3000!');
+app.listen(3000, function(){
+  console.log('PS Project Running on port 3000!');
+});
